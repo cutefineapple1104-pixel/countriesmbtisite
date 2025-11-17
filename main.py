b1e52ad90da676fcdb3f8acb91739b634878275a
cutefineapple1104-pixel <cutefineapple1104@gmail.com>
2025-11-17 15:30:44 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="약수 계산기", page_icon="📘")
+
+st.title("📘 약수 계산 웹앱")
+st.write("왼쪽 메뉴에서 원하는 기능을 선택하세요!")
+
+st.subheader("이 앱에서 할 수 있는 것")
+st.write("- 숫자의 약수 구하기")
+st.write("- 소수(prime number)인지 확인하기")
+st.write("- 만든 사람 정보 보기")
